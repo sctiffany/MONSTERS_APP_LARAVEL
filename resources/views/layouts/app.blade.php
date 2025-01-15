3b731adb88af7ce971def11b95877b2c160e588e
Tiffany <tiffany.schinelli@gmail.com>
2025-01-15 17:03:38 +0100

    Mise en place du template avec partials (tj en test avec types.index)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Monsters</title>
+    @include('layouts.partials._head')
 </head>
 
-<body>
-    <h1>Mon beau site</h1>
-    <hr>
-    @yield('content')
+<body class="bg-gray-800 text-white font-sans">
+    <!-- Header -->
+    @include('layouts.partials._header')
+
+    <!-- Main Content -->
+    @include('layouts.partials._main')
+
+    <!-- Footer -->
+    @include('layouts.partials._footer')
 </body>
 
 </html>
---
+++ resources/views/layouts/partials/_main.blade.php
@@ -0,0 +1,9 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12">
+    <main class="w-full md:w-3/4 p-4">
+        <!-- Section Monstre Aléatoire -->
+        @yield('content')
+    </main>
+
+    <!-- Sidebar -->
+    @include('layouts.partials._aside')
+</div>
