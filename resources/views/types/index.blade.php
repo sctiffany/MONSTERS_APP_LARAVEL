7514f4019ae5377dd26664f49e409424793cc060
Tiffany <tiffany.schinelli@gmail.com>
2025-01-16 11:08:10 +0100

    Test Render

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -9,4 +9,20 @@
             </li>
         @endforeach
     </ul>
+
+    <h2>Liste des monstres</h2>
+    <ul>
+        @foreach ($types as $type)
+            <li>
+                <strong>{{ $type->name }}</strong> <!-- Affiche le nom du type -->
+                <ul>
+                    @foreach ($type->monsters as $monster)
+                        <li>
+                            <a href="">{{ $monster->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 @endsection
